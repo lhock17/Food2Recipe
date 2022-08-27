4ab782252ce08dad674a206f7510de6b22825f0f
Levi Hockey <levi.hockey@gmail.com>
2022-08-27 16:52:57 +1000

    Added Flask Server

---
+++ server.py
@@ -0,0 +1,47 @@
+import os
+import re
+from flask import Flask, flash, request, redirect, url_for
+from recog import get_recipe
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            # return redirect(url_for('upload_file', name=filename))
+            recipe = get_recipe(file_path)
+            if not recipe:
+                recipe = "Couldn't find a recipe"
+            return recipe
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
